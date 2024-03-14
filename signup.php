4e5c38113c2d130bf3b938bd33eb5634a76692c9
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-14 21:02:28 +0200

    creating the signup API

---
+++ signup.php
@@ -0,0 +1,29 @@
+<?php
+include ("connection.php");
+ 
+ 
+$name = $_POST['name'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+ 
+$check_email_username = $mysqli->prepare('select email, username from users where email=? or username=?');
+$check_email_username ->bind_param('ss', $email,$username);
+$check_email_username ->execute();
+$check_email_username ->store_result();
+$email__username_exists = $check_email_username ->num_rows();
+
+if ($email__username_exists == 0) {
+    $hashed_password=password_hash($password, PASSWORD_BCRYPT);
+    $query = $mysqli->prepare('insert into users(name,username,email,password) values(?,?,?,?);');
+    $query->bind_param('ssss', $name, $username,$email,$hashed_password);
+    $query->execute();
+    $response['status'] = "success";
+
+} else {
+    $response["status"] = "user already exists";
+
+}
+echo json_encode($response);
+
+?>
