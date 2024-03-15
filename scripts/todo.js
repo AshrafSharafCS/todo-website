4796fd21ce7bacc4e475cc3154590b7b5384b7be
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-15 11:52:26 +0200

    adding the delete todo function

--- scripts/todo.js
+++ scripts/todo.js
@@ -14,14 +14,14 @@ const addTodo = async (formdata) => {
     console.log(error);
   }
 };
-
-const addTododata = new FormData();
-
-addTododata.append("description", "this is my new todo");
-
+// const addTododata = new FormData();
+// addTododata.append("description", "this is my new todo");
 // addTodo(addTododata).then((response) => {
 //   console.log(response);
 // });
+
+
+
 
 // Read todo
 const readTodo = async () => {
@@ -35,7 +35,35 @@ const readTodo = async () => {
     console.log(error);
   }
 };
-
 // readTodo().then((response) => {
 //   console.log(response);
 // });
+
+
+
+
+// Delete todo 
+const deleteTodo = async (formdata) => {
+  try {
+    const result = await fetch(
+      "http://localhost/todo-website/php/deleteTodo.php",
+      {
+        body: formdata,
+        method: "POST",
+      }
+    );
+
+    return await result.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+const deleteTododata = new FormData();
+deleteTododata.append("todo-id", 15);
+deleteTodo(deleteTododata).then((response) => {
+console.log(response);
+});
+
+
+
+
