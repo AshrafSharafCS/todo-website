0c912969df1ce42bcc09dcbd29a2af4ff18b6627
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-15 11:22:51 +0200

    adding read todos function

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,18 +1,41 @@
+// Create todo
 const addTodo = async (formdata) => {
-    try {
-      const result = await fetch("http://localhost/todo-website/php/createTodo.php", {
+  try {
+    const result = await fetch(
+      "http://localhost/todo-website/php/createTodo.php",
+      {
         body: formdata,
         method: "POST",
-      });
-  
-      return await result.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const addTododata = new FormData();
-  addTododata.append("description", "this is my new todo");
-  addTodo(addTododata).then((response) => {
-    console.log(response);
-  });
-  
+      }
+    );
+
+    return await result.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addTododata = new FormData();
+
+addTododata.append("description", "this is my new todo");
+
+// addTodo(addTododata).then((response) => {
+//   console.log(response);
+// });
+
+// Read todo
+const readTodo = async () => {
+  try {
+    const result = await fetch("http://localhost/todo-website/php/readTodo.php", {
+      method: "GET",
+    });
+
+    return await result.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// readTodo().then((response) => {
+//   console.log(response);
+// });
